afebaa28b6c5b3720e1fae6f0eb9795dfa02241a
Olufemi Victor <60588823+osinkolu@users.noreply.github.com>
2021-09-05 01:44:44 +0200

    remove string error by forcing float

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def check_status():
     print(int_features)
     final=[np.array(int_features)]
     prediction=model.predict(final)
-    output= abs(prediction[0])
+    output= abs(float(prediction[0]))
     return render_template("carbon_monitor.html",prediction_text="The estimated value of carboon in MT is {}".format(output))
 if __name__ =="__main__":
     app.run()
